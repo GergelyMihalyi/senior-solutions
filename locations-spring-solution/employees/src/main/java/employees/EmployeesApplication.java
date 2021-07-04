74fc17c984fe733ec4917d479b3e4182a32e7a75
GergelyMihalyi <33895800+GergelyMihalyi@users.noreply.github.com>
2021-07-04 22:45:57 +0200

    employees-m02-l04-status

--- locations-spring-solution/employees/src/main/java/employees/EmployeesApplication.java
+++ locations-spring-solution/employees/src/main/java/employees/EmployeesApplication.java
@@ -1,5 +1,6 @@
 package employees;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,5 +17,9 @@ public class EmployeesApplication {
 	@Bean
 	public ModelMapper modelMapper(){
 		return new ModelMapper();
+	}
+	@Bean
+	public ObjectMapper objectMapper(){
+		return new ObjectMapper().findAndRegisterModules();
 	}
 }
--- locations-spring-solution/employees/src/main/java/employees/EmployeesController.java
+++ locations-spring-solution/employees/src/main/java/employees/EmployeesController.java
@@ -1,7 +1,13 @@
 package employees;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.zalando.problem.Problem;
+import org.zalando.problem.Status;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,6 +32,7 @@ public class EmployeesController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public EmployeeDto createEmployee(@RequestBody CreateEmployeeCommand command) {
         return employeesService.createEmployee(command);
     }
@@ -36,7 +43,23 @@ public class EmployeesController {
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteEmployee(@PathVariable("id") long id){
         employeesService.deleteEmployee(id);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<Problem> handleNotFound(IllegalArgumentException iae){
+    Problem problem = Problem.builder()
+            .withType(URI.create("meployees/not-found"))
+            .withTitle("not found")
+            .withStatus(Status.NOT_FOUND)
+            .withDetail(iae.getMessage())
+            .build();
+    return ResponseEntity
+            .status(HttpStatus.NOT_FOUND)
+            .contentType(MediaType.APPLICATION_PROBLEM_JSON)
+            .body(problem);
     }
 }
